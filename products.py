59ab2fe475ee875f8322c4e9a9b0bc530d9cd806
suhunlin <lin.suhun@msa.hinet.net>
2022-04-26 09:21:26 +0800

    add save function

--- products.py
+++ products.py
@@ -1,22 +1,22 @@
 products = []
+def save_function(products_data):
+    try:
+        with open("product.txt","w") as file:
+            for product in products_data:
+                str = "商品："+product[0] + ",價格：" + product[1] + "\n"
+                file.write(str)
+    except Exception as error_message:
+        print(error_message)
 while True:
-    name = input("Please input product name:")
+    name = input("Please input product name(press q to exit):")
     if name == "q":
+        print(products)
+        save_function(products)
         break
     price = input("Please input product price:")
     # p = [name,price]
     products.append([name,price])
-for product in products:
-    str = '商品名稱：'+product[0]+',商品價格：'+product[1]
-    print(str)
 
 
 
-try:
-    with open('products.txt','w') as file:
-        for product in products:
-            str = "商品名稱："+product[0]+",商品價格："+product[1]+"\n"
-            file.write(str)
 
-except Exception as e:
-    print(e)
