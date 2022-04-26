e6394629d54825f13c3958bc780a08759bfce3f7
suhunlin <lin.suhun@msa.hinet.net>
2022-04-26 09:56:59 +0800

    add table to product

--- products.py
+++ products.py
@@ -2,6 +2,7 @@ products = []
 def save_function(products_data):
     try:
         with open("product.csv","w") as file:
+            file.write("商品,價格\n")
             for product in products_data:
                 str = product[0] + "," + product[1] + "\n"
                 file.write(str)
