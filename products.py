53a42e6ccb380d6e67286079e84c8d5f891d540d
suhunlin <lin.suhun@msa.hinet.net>
2022-04-26 14:46:35 +0800

    add load function

--- products.py
+++ products.py
@@ -1,22 +1,40 @@
+
 products = []
-def save_function(products_data):
+def load_function():
     try:
-        with open("product.csv","w",encoding='utf-8') as file:
-            file.write("商品,價格\n")
-            for product in products_data:
-                str = product[0] + "," + product[1] + "\n"
-                file.write(str)
+        with open('product.csv', 'r', encoding='utf-8') as file:
+            for line in file:
+                name, price = line.strip().split(',') #1.先用strip把讀到的資料\n刪掉2.再用split(',')把資料依照','分隔
+                                                      # 3.把分隔後的資料分別存到name跟price裡
+                products.append([name,price])
+        print(products)
     except Exception as error_message:
         print(error_message)
-while True:
-    name = input("Please input product name(press q to exit):")
-    if name == "q":
-        print(products)
-        save_function(products)
-        break
-    price = input("Please input product price:")
-    # p = [name,price]
-    products.append([name,price])
+def store_function(list_data):
+    try:
+        with open('product.csv','w',encoding='utf-8') as file:
+            if '商品' not in list_data[0]:
+                file.write('商品' + ',' + '價格'+'\n')
+                print('write 商品')
+            for product in list_data:
+                file.write(product[0] + ',' + product[1]+'\n')
+    except Exception as error_message:
+        print(error_message)
+def main():
+    load_function()
+    while True:
+        product = input('請輸入商品名稱(結束請輸入q):')
+        if product == 'q':
+            if not products:  # 如果products list裡面是空清單的話
+                print('空清單，離開!!!')
+                break
+            else:
+                store_function(products)
+            break
+        price = input('請輸入商品價格:')
+        products.append([product, price])
+if __name__ == '__main__':
+    main()
 
 
 
