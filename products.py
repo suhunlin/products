79218bbcb4005521124a699ccf11722f439b42e6
suhunlin <lin.suhun@msa.hinet.net>
2022-04-27 16:05:28 +0800

    code refactor

--- products.py
+++ products.py
@@ -1,20 +1,19 @@
+import os
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱(\'q\'離開程式):')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格:')
+        products.append([name,price])
+    return products
 
-def sorte_product(product_list):
-    try:
-        with open('products.csv','w',encoding='utf-8') as file:
-            file.write('產品'+','+'價格'+'\n')
-            for product in product_list:
-                file.write(product[0]+','+product[1]+'\n')
-
-    except Exception as error_message:
-        print(error_message)
-
-def load_product():
+def load_product(filename):
     products = []
     try:
-        with open('products.csv','r',encoding='utf-8') as file:
+        with open(filename,'r',encoding='utf-8') as file:
             for line in file:
-                if '產品,價格' in line:
+                if '商品,價格' in line:
                     continue
                 name,price = line.strip().split(',')
                 products.append([name,price])
@@ -22,23 +21,37 @@ def load_product():
         print(error_message)
     return products
 
-def user_input_product(product_list):
-    products = product_list
-    while True:
-        name = input('請輸入產品名稱(結束輸入q):')
-        if name == 'q':
-            if not products:
-                print('No product data!!!')
-            else:
-                sorte_product(products)
-            break
-        price = input('請輸入產品價格：')
-        products.append([name,price])
+def store_prodect(products,filename):
+    result = False
+    if not products or not filename:
+        return result
+    try:
+        with open(filename,'w',encoding='utf-8') as file:
+            file.write('商品' + ',' + '價格' + '\n')
+            for project in products:
+                file.write(project[0] + ',' + project[1] + '\n')
+            result = True
+    except Exception as error_message:
+        print(error_message)
+    return result
+
+def print_product_info(products):
+    if not products:
+        print('沒有任何產品資訊!!!')
+    for product in products:
+        if '商品,價格' in product:
+            continue
+        print(product)
 
 def main():
-    products = load_product()
-    print(products)
-    user_input_product(products)
+    filename = 'products.csv'
+    if os.path.exists(filename):
+        products = load_product(filename)
+    else:
+        products = []
+    products = user_input(products)
+    print_product_info(products)
+    store_prodect(products, filename)
 
 if __name__ == '__main__':
     main()
